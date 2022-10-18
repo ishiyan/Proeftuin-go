0a257406e5997f9705cbc46f22479786f2617d11
ivas <ivas@saxobank.com>
2022-10-18 07:15:53 +0200

    finished combining

--- combine-csv/main.go
+++ combine-csv/main.go
@@ -35,28 +35,32 @@ type managementFee struct {
 	gros          string
 	net           string
 	btw           string
+	used          bool
 }
 
 type promotion struct {
 	accountNumber string
 	gros          float64
 	net           float64
+	used          bool
 }
 
 type correction struct {
 	accountNumber string
 	gros          float64
 	net           float64
+	used          bool
 }
 
 type mapping struct {
 	accountNumber string
 	accountId     string
+	used          bool
 }
 
-func readManagementFee() map[string]managementFee {
+func readManagementFee() map[string]*managementFee {
 	csv := readCsvFile("management-fee-2021.csv", 5)
-	m := map[string]managementFee{}
+	m := map[string]*managementFee{}
 
 	for _, e := range csv {
 		v := managementFee{
@@ -68,18 +72,18 @@ func readManagementFee() map[string]managementFee {
 		}
 
 		if _, ok := m[v.accountNumber]; ok {
-			log.Fatal("Duplicate account in management fees: " + v.accountNumber)
+			log.Fatal("Duplicate account number in management fees: " + v.accountNumber)
 		}
 
-		m[v.accountNumber] = v
+		m[v.accountNumber] = &v
 	}
 
 	return m
 }
 
-func readPromotions() map[string]promotion {
-	csv := readCsvFile("promotions-2021-extract.csv", 3)
-	m := map[string]promotion{}
+func readPromotions() map[string]*promotion {
+	csv := readCsvFile("promotions-2021.csv", 3)
+	m := map[string]*promotion{}
 
 	for _, e := range csv {
 		gros, _ := strconv.ParseFloat(e[1], 64)
@@ -95,16 +99,16 @@ func readPromotions() map[string]promotion {
 				net:           net,
 			}
 
-			m[v.accountNumber] = v
+			m[v.accountNumber] = &v
 		}
 	}
 
 	return m
 }
 
-func readCorrections() map[string]correction {
-	csv := readCsvFile("corrections-2021-extracted.csv", 3)
-	m := map[string]correction{}
+func readCorrections() map[string]*correction {
+	csv := readCsvFile("corrections-2021.csv", 3)
+	m := map[string]*correction{}
 
 	for _, e := range csv {
 		gros, _ := strconv.ParseFloat(e[1], 64)
@@ -120,16 +124,16 @@ func readCorrections() map[string]correction {
 				net:           net,
 			}
 
-			m[v.accountNumber] = v
+			m[v.accountNumber] = &v
 		}
 	}
 
 	return m
 }
 
-func readMapping() map[string]mapping {
-	csv := readCsvFile("mapping-extracted.csv", 2)
-	m := map[string]mapping{}
+func readMapping() map[string]*mapping {
+	csv := readCsvFile("mapping-2021.csv", 2)
+	m := map[string]*mapping{}
 
 	for _, e := range csv {
 		if _, ok := m[e[0]]; !ok {
@@ -138,7 +142,7 @@ func readMapping() map[string]mapping {
 				accountId:     e[1],
 			}
 
-			m[v.accountNumber] = v
+			m[v.accountNumber] = &v
 		}
 	}
 
@@ -151,31 +155,53 @@ func main() {
 	pro := readPromotions()
 	mf := readManagementFee()
 
-	fmt.Println(mp)
-	fmt.Println(cor)
-	fmt.Println(pro)
-	fmt.Println(mf)
+	// fmt.Println(mp)
+	// fmt.Println(cor)
+	// fmt.Println(pro)
+	// fmt.Println(mf)
 
 	fmt.Println("AccountNumber;AccountId;Year;BrutoFee;NetFee;VAT;Correction;Promotion")
 
 	for _, emf := range mf {
 		var id = "xxx"
-		if _, ok := mp[emf.accountNumber]; ok {
+		if e1, ok := mp[emf.accountNumber]; ok {
+			id = e1.accountId
+			e1.used = true
+		} else {
 			log.Println("No account id for account number " + emf.accountNumber)
-			id = mp[emf.accountNumber].accountId
 		}
 
 		var p float64
 		var c float64
 
-		if _, ok := pro[emf.accountNumber]; ok {
-			p = pro[emf.accountNumber].net
+		if e2, ok := pro[emf.accountNumber]; ok {
+			p = e2.net
+			e2.used = true
 		}
 
-		if _, ok := cor[emf.accountNumber]; ok {
-			c = cor[emf.accountNumber].net
+		if e3, ok := cor[emf.accountNumber]; ok {
+			c = e3.net
+			e3.used = true
 		}
 
 		fmt.Printf("%v;%v;%v;%v;%v;%v;%v;%v\n", emf.accountNumber, id, emf.year, emf.gros, emf.net, emf.btw, c, p)
+	}
+
+	for _, e := range pro {
+		if !e.used {
+			log.Println("Promotion not used: account number " + e.accountNumber)
+		}
+	}
+
+	for _, e := range cor {
+		if !e.used {
+			log.Println("Correction not used: account number " + e.accountNumber)
+		}
+	}
+
+	for _, e := range mp {
+		if !e.used {
+			log.Println("Mapping not used: account number " + e.accountNumber)
+		}
 	}
 }
